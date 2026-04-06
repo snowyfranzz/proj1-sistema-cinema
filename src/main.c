2195e79110f68dbcd161ca10284a5ff24beb886a
RCalosena <gntapia11@gmail.com>
2026-04-06 14:12:43 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,9 +2,13 @@
 # include <stdlib.h>
 # include <locale.h>
 
+# define fileiras 10
+# define assentos 8
+
 int main() {
 
 
+    printf("fileiras: %d assentos: %d\n\n", fileiras, assentos);
 
     system("pause");
     return 0;
