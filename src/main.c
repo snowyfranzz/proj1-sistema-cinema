3709f502b85771819d30d2d3b9b7c6d2bd13a6a7
Fran <fran.granzier@gmail.com>
2026-04-06 14:24:28 -0300

    fix: implementação do setlocale (biblioteca locale.h)

--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
 
+<<<<<<< Updated upstream
+=======
+# define fileiras 10
+# define assentos 8
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+
+    printf("fileiras: %d assentos: %d\n\n", fileiras, assentos);
+
+    system("pause");
+    return 0;
+}
+>>>>>>> Stashed changes
