2f9abf6b4f7eb54193035c625462af17ce278258
RCalosena <gntapia11@gmail.com>
2026-04-06 14:42:46 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,16 +6,18 @@
 # define assentos 8
 
 int main() {
-    setlocale(LC_ALL, "portuguese");
     int i, j, teatro[fileiras][assentos];
 
+    setlocale(LC_ALL, "portuguese");
 
+    //coloca cada valor como 0
     for (i = 0; i < fileiras; i++) {
         for (j = 0; j < assentos; j++) {
             teatro[i][j] = 0;
         }
     }
 
+    //imprime matriz
     for (i = 0; i < fileiras; i++) {
             printf("\n");
         for (j = 0; j < assentos; j++) {
@@ -23,6 +25,7 @@ int main() {
         }
     }
 
+    //imprime numero de linhas/colunas
     printf("\n\nfileiras: %d assentos: %d\n\n", fileiras, assentos);
 
     system("pause");
