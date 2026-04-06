642a2664ea16bf7bbbe108293615d17c041b8f3f
RCalosena <gntapia11@gmail.com>
2026-04-06 14:40:47 -0300

    awd
    define and print matrix

--- src/main.c
+++ src/main.c
@@ -7,8 +7,23 @@
 
 int main() {
     setlocale(LC_ALL, "portuguese");
+    int i, j, teatro[fileiras][assentos];
 
-    printf("fileiras: %d assentos: %d\n\n", fileiras, assentos);
+
+    for (i = 0; i < fileiras; i++) {
+        for (j = 0; j < assentos; j++) {
+            teatro[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < fileiras; i++) {
+            printf("\n");
+        for (j = 0; j < assentos; j++) {
+            printf("%d ", teatro[i][j]);
+        }
+    }
+
+    printf("\n\nfileiras: %d assentos: %d\n\n", fileiras, assentos);
 
     system("pause");
     return 0;
