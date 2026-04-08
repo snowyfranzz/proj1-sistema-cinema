aca924d65c7ed8d054a0fd4a1c5a8a66e2dc7e7f
RCalosena <gntapia11@gmail.com>
2026-04-08 13:06:27 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 # define assentos 8
 
 int main() {
-    int i, j, teatro[fileiras][assentos];
+    int teatro[fileiras][assentos], i, j, totalLivres;
 
     setlocale(LC_ALL, "portuguese");
 
@@ -17,16 +17,71 @@ int main() {
         }
     }
 
+    do {
+
+    system("cls");
+
+    //imprime numero de linhas/colunas
+    printf("Fileiras: %d | Assentos: %d\n\n", fileiras, assentos);
+
     //imprime matriz
     for (i = 0; i < fileiras; i++) {
+        printf("\n\t%2d  ", i + 1);
         for (j = 0; j < assentos; j++) {
             printf("%d ", teatro[i][j]);
         }
-        printf("\n");
+        printf(" %d", i + 1);
     }
 
-    //imprime numero de linhas/colunas
-    printf("\n\nfileiras: %d assentos: %d\n\n", fileiras, assentos);
+        // escolha a fileira
+        printf("\n\nEscolha a sua fileira: ");
+        scanf("%d", &i);
+    } while (i < 1 || i > fileiras);
+
+    i--;
+
+    do {
+
+    system("cls");
+
+    //imprime fileira
+    printf("\n\t%2d  ", i + 1);
+    for (j = 0; j < assentos; j++) {
+        printf("%d ", teatro[i][j]);
+    }
+    printf(" %d", i + 1);
+
+    // escolha a fileira
+        printf("\n\nEscolha a sua Cadeira: ");
+        scanf("%d", &j);
+
+    } while (j < 1 || j > assentos);
+
+
+    j--;
+
+    teatro[i][j] = 1;
+
+    //imprime matriz final
+    for (i = 0; i < fileiras; i++) {
+        printf("\n\t%2d  ", i + 1);
+        for (j = 0; j < assentos; j++) {
+            printf("%d ", teatro[i][j]);
+        }
+        printf(" %d", i + 1);
+    }
+
+    for (i = 0; i < fileiras; i++) {
+        for (j = 0; j < assentos; j++) {
+            if (teatro[i][j] == 0) {
+                totalLivres++;
+            }
+        }
+    }
+
+    printf("\n\n");
+
+    printf("Livres: %d\nOcupados: %d\n\n", totalLivres, (fileiras * assentos) - totalLivres);
 
     system("pause");
     return 0;
