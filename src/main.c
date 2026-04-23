6e9b6450919337cc31ca60e44fe0fc6205401ab5
RCalosena <gntapia11@gmail.com>
2026-04-23 09:57:00 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,13 +13,13 @@ int main() {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 1, 0, 0, 0, 0},
         {0, 0, 1, 0, 1, 0, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 1, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0}
     };
-    int entropy, entropyLin, minEnt = 999, i, j, k, ingressos, minEsquerdo = 0, fil = 0;
+    int pior, piorLinha, menosPior = 999, i, j, k, ingressos, minEsquerdo = 0, fil = 0;
     setlocale(LC_ALL, "portuguese");
 
     ingressos = 4;
@@ -33,54 +33,54 @@ int main() {
     }
 
     for (i = 0; i < fileiras; i++) {
-            entropyLin = 0;
+            piorLinha = 0;
 
             if (i < metadeLin) {
-                entropyLin += (metadeLin - (i + 1));
+                piorLinha += (piorLinha - (i + 1));
             } else {
-                entropyLin += (i - metadeLin);
+                piorLinha += (i - metadeLin);
             }
 
         for (j = 0; j < assentos; j++) {
-            entropy = 0;
+            pior = 0;
 
             for (k = 0; k < ingressos; k++) {
                 if (teatro[i][j + k] == 1 || j + k >= assentos) {
-                    entropy = 999;
+                    pior = 999;
                     break;
                 }
 
-                entropy += entropyLin;
+                pior += piorLinha;
 
                 if (j + k < metadeCol) {
-                    entropy += (metadeCol - ((j + k) + 1));
+                    pior += (metadeCol - ((j + k) + 1));
                 } else {
-                    entropy += ((j + k) - metadeCol);
+                    pior += ((j + k) - metadeCol);
                 }
 
                 if (i + 1 < fileiras && teatro[i + 1][j + k] == 1) {
-                    entropy++;
+                    pior++;
                 }
                 if (i - 1 >= 0 && teatro[i - 1][j + k] == 1) {
-                    entropy++;
+                    pior++;
                 }
                 if (k + 1 == ingressos && (j + k) + 1 < assentos && teatro[i][(j + k) + 1] == 1) {
-                    entropy += 2;
+                    pior += 2;
                 }
                 if (k == 0 && (j + k) - 1 >= 0 && teatro[i][(j + k) - 1] == 1) {
-                    entropy += 2;
+                    pior += 2;
                 }
             }
 
-            if (entropy < minEnt) {
-                minEnt = entropy;
+            if (pior < menosPior) {
+                menosPior = pior;
                 fil = i + 1;
                 minEsquerdo = j + 1;
             }
         }
     }
 
-    if (minEnt < 999) {
+    if (menosPior < 999) {
         printf("\nFileira: %d\n", fil);
         printf("Assentos: %d..%d\n", minEsquerdo, minEsquerdo + (ingressos - 1));
     } else {
