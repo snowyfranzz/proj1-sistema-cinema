ee55b66f62b58a8382052bb84645fdd9173c35fd
RCalosena <gntapia11@gmail.com>
2026-04-23 10:03:06 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int main() {
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0}
     };
-    int pior, piorLinha, menosPior = 999, i, j, k, ingressos, minEsquerdo = 0, fil = 0;
+    int incomodo, incomodoFileiras, menosIncomodo = 999, i, j, k, ingressos, minEsquerdo = 0, fil = 0;
     setlocale(LC_ALL, "portuguese");
 
     ingressos = 4;
@@ -33,54 +33,54 @@ int main() {
     }
 
     for (i = 0; i < fileiras; i++) {
-            piorLinha = 0;
+            incomodoFileiras = 0;
 
             if (i < metadeLin) {
-                piorLinha += (piorLinha - (i + 1));
+                incomodoFileiras += (incomodoFileiras - (i + 1));
             } else {
-                piorLinha += (i - metadeLin);
+                incomodoFileiras += (i - metadeLin);
             }
 
         for (j = 0; j < assentos; j++) {
-            pior = 0;
+            incomodo = 0;
 
             for (k = 0; k < ingressos; k++) {
                 if (teatro[i][j + k] == 1 || j + k >= assentos) {
-                    pior = 999;
+                    incomodo = 999;
                     break;
                 }
 
-                pior += piorLinha;
+                incomodo += incomodoFileiras;
 
                 if (j + k < metadeCol) {
-                    pior += (metadeCol - ((j + k) + 1));
+                    incomodo += (metadeCol - ((j + k) + 1));
                 } else {
-                    pior += ((j + k) - metadeCol);
+                    incomodo += ((j + k) - metadeCol);
                 }
 
                 if (i + 1 < fileiras && teatro[i + 1][j + k] == 1) {
-                    pior++;
+                    incomodo++;
                 }
                 if (i - 1 >= 0 && teatro[i - 1][j + k] == 1) {
-                    pior++;
+                    incomodo++;
                 }
                 if (k + 1 == ingressos && (j + k) + 1 < assentos && teatro[i][(j + k) + 1] == 1) {
-                    pior += 2;
+                    incomodo += 2;
                 }
                 if (k == 0 && (j + k) - 1 >= 0 && teatro[i][(j + k) - 1] == 1) {
-                    pior += 2;
+                    incomodo += 2;
                 }
             }
 
-            if (pior < menosPior) {
-                menosPior = pior;
+            if (incomodo < menosIncomodo) {
+                menosIncomodo = incomodo;
                 fil = i + 1;
                 minEsquerdo = j + 1;
             }
         }
     }
 
-    if (menosPior < 999) {
+    if (menosIncomodo < 999) {
         printf("\nFileira: %d\n", fil);
         printf("Assentos: %d..%d\n", minEsquerdo, minEsquerdo + (ingressos - 1));
     } else {
