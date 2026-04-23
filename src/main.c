9a633452ccb0bc722c58ad0f20f98478f7c81434
RCalosena <gntapia11@gmail.com>
2026-04-23 08:18:09 -0300

    Algoritmo sem coisas opcionais

--- src/main.c
+++ src/main.c
@@ -8,98 +8,24 @@
 # define metadeCol assentos / 2
 
 int main() {
-    int teatro[fileiras][assentos] = {0}, cont = 1, entropy, entropyLin, minEnt = 999, i, j, k, ingressos, c, minEsquerdo = 0, fil = 0;
+    int teatro[fileiras][assentos] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 1, 0, 1, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int entropy, entropyLin, minEnt = 999, i, j, k, ingressos, minEsquerdo = 0, fil = 0;
     setlocale(LC_ALL, "portuguese");
 
-    do {
-        system("cls");
-
-        for (i = 0; i < fileiras; i++) {
-            for (j = 0; j < assentos; j++) {
-                printf("%2d ", teatro[i][j]);
-            }
-            printf("\n");
-        }
-
-        printf("\nLinha: ");
-        if (scanf("%d", &i) == 0) {
-            i = 0;
-        }
-        while((c = getchar()) != '\n' && c != EOF);
-
-        printf("\nColuna: ");
-        if (scanf("%d", &j) == 0) {
-            j = 0;
-        }
-        while((c = getchar()) != '\n' && c != EOF);
-
-        if (i > 0 && i <= fileiras && j > 0 && j <= assentos) {
-            i--;
-            j--;
-            teatro[i][j] = 11;
-        } else {
-            system("cls");
-            printf("\nINVÁLIDO\n\nContinuar? (1 ou 0): ");
-            scanf("%d", &cont);
-            while((c = getchar()) != '\n' && c != EOF);
-        }
-    } while (cont != 0);
-
-    do {
-        system("cls");
-        printf("\nNumero de pessoas: ");
-        if (scanf("%d", &ingressos) == 0) {
-            ingressos = 0;
-        }
-        while((c = getchar()) != '\n' && c != EOF);
-    } while (!ingressos);
-
-    system("cls");
-
-    for (i = 0; i < fileiras; i++) {
-        for (j = 0; j < assentos; j++) {
-
-            if (teatro[i][j] == 11) {
-                continue;
-            }
-
-            entropy = 0;
-
-            if (i < metadeLin) {
-                entropy += (metadeLin - (i + 1));
-            } else {
-                entropy += (i - metadeLin);
-            }
-            if (i + 1 < fileiras) {
-                if (teatro[i + 1][j] == 11) {
-                    entropy++;
-                }
-            }
-            if (i - 1 >= 0) {
-                if (teatro[i - 1][j] == 11) {
-                    entropy ++;
-                }
-            }
-
-            if (j < metadeCol) {
-                entropy += (metadeCol - (j + 1));
-            } else {
-                entropy += (j - metadeCol);
-            }
-            if (j + 1 < assentos) {
-                if (teatro[i][j + 1] == 11) {
-                    entropy += 2;
-                }
-            }
-            if (j - 1 >= 0) {
-                if (teatro[i][j - 1] == 11) {
-                    entropy += 2;
-                }
-            }
-
-            teatro[i][j] = entropy;
-        }
-    }
+    ingressos = 4;
+    printf("\nIngressos: %d\n\n", ingressos);
 
     for (i = 0; i < fileiras; i++) {
         for (j = 0; j < assentos; j++) {
@@ -121,17 +47,17 @@ int main() {
             entropy = 0;
 
             for (k = 0; k < ingressos; k++) {
-                if (teatro[i][j + k] == 11 || j + k >= assentos) {
+                if (teatro[i][j + k] == 1 || j + k >= assentos) {
                     entropy = 999;
                     break;
                 }
 
                 entropy += entropyLin;
 
-                if (i + 1 < fileiras && teatro[i + 1][j + k] == 11) {
+                if (i + 1 < fileiras && teatro[i + 1][j + k] == 1) {
                     entropy++;
                 }
-                if (i - 1 >= 0 && teatro[i - 1][j + k] == 11) {
+                if (i - 1 >= 0 && teatro[i - 1][j + k] == 1) {
                     entropy++;
                 }
 
@@ -140,10 +66,10 @@ int main() {
                 } else {
                     entropy += ((j + k) - metadeCol);
                 }
-                if (k + 1 == ingressos && (j + k) + 1 < assentos && teatro[i][(j + k) + 1] == 11) {
+                if (k + 1 == ingressos && (j + k) + 1 < assentos && teatro[i][(j + k) + 1] == 1) {
                     entropy += 2;
                 }
-                if (k == 0 && (j + k) - 1 >= 0 && teatro[i][(j + k) - 1] == 11) {
+                if (k == 0 && (j + k) - 1 >= 0 && teatro[i][(j + k) - 1] == 1) {
                     entropy += 2;
                 }
             }
@@ -157,13 +83,11 @@ int main() {
     }
 
     if (minEnt < 999) {
-        printf("\nmenorsoma: %d\n\n", minEnt);
-        printf("Fileira: %d\n\n", fil);
-        printf("Assentos: %d..%d\n\n", minEsquerdo, minEsquerdo + (ingressos - 1));
+        printf("\nFileira: %d\n", fil);
+        printf("Assentos: %d..%d\n", minEsquerdo, minEsquerdo + (ingressos - 1));
     } else {
-        printf("\nNão existem recomendações\n\n");
+        printf("\nNão existem recomendações\n");
     }
 
-    system("pause");
     return 0;
 }
